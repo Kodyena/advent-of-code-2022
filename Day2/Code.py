d29b758459ed22f83fc1ad554624ae324e5d69e6
Tristan Downing <tristan.downing@nimbleapproach.com>
2022-12-02 15:05:09 +0000

    Functionalised day 2

--- Day2/Code.py
+++ Day2/Code.py
@@ -1,27 +1,22 @@
-outcomes = {
-    "A Y": 8,
-    "A Z": 3,
-    "B X": 1,
-    "B Z": 9,
-    "C X": 7,
-    "C Y": 2,
-    "A X": 4,
-    "B Y": 5,
-    "C Z": 6
+char_to_val = {
+    "A": 1,
+    "B": 2,
+    "C": 3,
+    "X": 1,
+    "Y": 2,
+    "Z": 3
 }
 
-outcomesTwo = {
-    "A X": 3,
-    "A Y": 4,
-    "A Z": 8,
-    "B X": 1,
-    "B Y": 5,
-    "B Z": 9,
-    "C X": 2,
-    "C Y": 6,
-    "C Z": 7
-}
+def result(x, y):
+    return (1 + (y - x)) % 3 * 3
+
+def getChoice(x, y):
+    return (y - 3 + x) % 3 + 1
 
 with open("Day2\Data\Guide.txt") as f:
-    results = f.read().split("\n")
-    print("Score: {}".format(sum(map(lambda l: outcomesTwo[l], results))))
+    results = [[char_to_val[c] for c in line.split(" ")] for line in f.read().split("\n")]
+    score_game_one = sum((map(lambda x: result(x[0], x[1]) + x[1], results)))
+    score_game_two = sum((map(lambda x: result(x[0], getChoice(x[0], x[1])) + getChoice(x[0], x[1]), results)))
+
+print("Score game 1: {}".format(score_game_one))
+print("Score game 2: {}".format(score_game_two)) 
